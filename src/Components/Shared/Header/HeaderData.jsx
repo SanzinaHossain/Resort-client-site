5458869052f80a63d92bb2dd870ff55d60cb7309
SanzinaHossain <sanzina67hossain@gmail.com>
2024-07-18 13:41:19 +0600

    file add

--- src/Components/Shared/Header/HeaderData.jsx
+++ src/Components/Shared/Header/HeaderData.jsx
@@ -1,6 +1,6 @@
 export const routes = [
   { id: 1, path: "/", name: "Home" },
-  { id: 2, path: "/about", name: "About" },
+  { id: 2, path: "/restaurant", name: "Restaurant" },
   { id: 3, path: "/rooms", name: "Rooms" },
   { id: 4, path: "/blogs", name: "Blogs" },
   { id: 5, path: "/contact", name: "Contact" },
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./Components/Screens/Home/Home.jsx"
 import Blog from "./Components/Screens/Blog/Blog.jsx"
+import Contact from "./Components/Screens/Contact/Contact.jsx"
+import Restaurant from "./Components/Screens/Restaurant/Restaurant.jsx"
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,6 +19,14 @@ const router = createBrowserRouter([
       {
         path: "/blogs",
         element: <Blog />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant",
+        element: <Restaurant />,
       },
     ],
   },
